c20672cd6c079f31db44f9e2583c00e464064040
lzrdblzzrd <32615368+lzrdblzzrd@users.noreply.github.com>
2019-07-16 18:19:09 +0300

    j

--- steamtextcenter/server/routes/api.js
+++
@@ -1,8 +0,0 @@
-import api from '../controllers/api.js'
-import koaRouter from 'koa-router'
-const router = koaRouter()
-
-router.post('/text/center', api.center)
-// router.get('/text/measure', api.hello)
-
-export default router

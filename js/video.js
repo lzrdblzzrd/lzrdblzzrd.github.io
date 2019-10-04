e3f396e52bed0f6f29cccc63e2c165e1eb064043
website <lzrdblzzrd@gmail.com>
2019-10-04 21:50:29 +0300

    Update

--- js/video.js
+++ js/video.js
@@ -8,9 +8,9 @@
     player = new YT.Player('ytplayer', {
         height: '1080',
         width: '1920',
-        videoId: '-nY_RiKWFoI',
+        videoId: '08Ydny8ZAzE',
         playerVars: {
-          playlist: '-nY_RiKWFoI',
+          playlist: '08Ydny8ZAzE',
           autoplay: 1,
           controls: 0,
           disablekb: 1,

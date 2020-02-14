074c75438c9470946316b0fcfc97169153ccfac0
lzrdblzzrd <32615368+lzrdblzzrd@users.noreply.github.com>
2020-02-14 20:14:01 +0300

    Update

--- js/video.js
+++ js/video.js
@@ -8,9 +8,9 @@
     player = new YT.Player('ytplayer', {
         height: '1080',
         width: '1920',
-        videoId: 'I53XXVrK-hE',
+        videoId: 'wg_qFp-q6dA',
         playerVars: {
-          playlist: 'I53XXVrK-hE',
+          playlist: 'wg_qFp-q6dA',
           autoplay: 1,
           controls: 0,
           disablekb: 1,
